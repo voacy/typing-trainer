84102a42e261f51845718cd8c7c3fd35a1c213b7
Vazgen <voacys@gmail.com>
2026-04-28 04:35:16 +0500

    add custom typing cursor

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import useTyping from "./features/typing/useTyping";
 import { getLetterClass, getActiveClass } from "./shared/lib";
 import useCapsLock from "./features/capsLock/useCapsLock";
 import { LockKeyhole } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
 	const { timer, timerStatus, startTimer } = useTimer();
@@ -14,6 +15,21 @@ function App() {
 		timerStatus,
 		startTimer,
 	);
+	const [cursorPos, setCursorPos] = useState({ top: 0, left: 0 });
+	const wrapperRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const el = document.querySelector(".active");
+
+		if (el && wrapperRef.current) {
+			const elPos = el.getBoundingClientRect();
+			const wrapperPos = wrapperRef.current.getBoundingClientRect();
+			setCursorPos({
+				top: elPos.top - wrapperPos.top,
+				left: elPos.left - wrapperPos.left,
+			});
+		}
+	}, [currentWordIndex, currentLetterIndex]);
 
 	return (
 		<>
@@ -33,22 +49,31 @@ function App() {
 						</span>
 					)}
 
-					<div className="text__wrapper">
+					<div className="text__wrapper" ref={wrapperRef}>
+						<span
+							className="cursor"
+							style={{ top: `${cursorPos.top}px`, left: `${cursorPos.left}px` }}
+						></span>
 						<ul className="text">
 							{words.map((word, wordIndex) => {
 								const isWordIncorrect =
 									wordIndex < currentWordIndex &&
 									letterStatuses[wordIndex].some((status) => status !== "correct");
+
+								const isLastletter =
+									words[currentWordIndex].length === currentLetterIndex &&
+									wordIndex === currentWordIndex;
 								return (
 									<li key={wordIndex} className={`word ${isWordIncorrect ? "incorrect-word" : ""}`}>
 										{word.split("").map((letter, letterIndex) => (
 											<span
 												key={letterIndex}
-												className={`${getLetterClass(letterStatuses, wordIndex, letterIndex)} ${getActiveClass(wordIndex, letterIndex, currentWordIndex, currentLetterIndex)} letter`}
+												className={`letter ${getLetterClass(letterStatuses, wordIndex, letterIndex)} ${getActiveClass(wordIndex, letterIndex, currentWordIndex, currentLetterIndex)}`}
 											>
 												{letter}
 											</span>
 										))}
+										{isLastletter && <span className="letter active"></span>}
 									</li>
 								);
 							})}
