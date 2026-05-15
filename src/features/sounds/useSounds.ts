a1f3bb42c8fb8e20826bcdbe06bf226f16de2382
Vazgen <voacys@gmail.com>
2026-05-15 23:46:37 +0500

    chore: adjust audio volume

--- src/features/sounds/useSounds.ts
+++ src/features/sounds/useSounds.ts
@@ -9,7 +9,7 @@ const useGameSounds = () => {
 	const [playClick] = useSound(clickSound, { volume: 0.2 });
 	const [playResult] = useSound(resultSound, { volume: 0.2 });
 	const [playCorrect] = useSound(correctSound, { volume: 0.5 });
-	const [playIncorrect] = useSound(incorrectSound, { volume: 0.2 });
+	const [playIncorrect] = useSound(incorrectSound, { volume: 0.5 });
 
 	return {
 		playClick,
