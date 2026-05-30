0cec76ddeec955158931c990776a270bfef0f19c
Vazgen <voacys@gmail.com>
2026-05-30 09:28:20 +0500

    feat: update 404 page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { HouseIcon } from "@phosphor-icons/react";
 import "./NotFoundPage.scss";
 
 const NotFoundPage = () => {
@@ -9,10 +10,10 @@ const NotFoundPage = () => {
 
 	return (
 		<div className="not-found">
-			<p className="not-found__subtitle">why are you here...</p>
-			<h1 className="not-found__title">404 </h1>
+			<h1 className="not-found__title">404</h1>
 			<p className="not-found__description">looks like this page doesn't exist</p>
 			<Link to="/" className="not-found__link">
+				<HouseIcon size={16} weight="fill" />
 				go home
 			</Link>
 		</div>
